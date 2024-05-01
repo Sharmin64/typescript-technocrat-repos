74fbbef6b26ca7b79a0481d30072c441beda9c16
Sharmin64 <lizaeatsbanana@gmail.com>
2024-05-02 00:48:31 +0600

    initial type script file done

---
+++ module1/src/1_4.ts
@@ -0,0 +1,23 @@
+//* Besic data types 
+
+// * String
+
+let firstName: string = 'Persian vay'
+
+// * number 
+
+let roll : number = 150
+// *boolean 
+ let isAdmin : boolean= true
+// *undefined 
+let x: undefined= undefined 
+// * null 
+let y: null= null
+
+
+
+// * Array 
+
+ let friends : string[]= [ 'rachel', 'konika', 'bonni', 'ana hathway']
+
+ let eligibleRollList : number[] = [ 34, 67 , 78 , 23]
